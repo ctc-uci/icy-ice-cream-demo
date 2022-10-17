f89c6c90801ae5b893c6608c9ac638348e8e2841
Jane Vo <vo.janehanh@gmail.com>
2022-10-17 14:39:06 -0700

    About page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 function App() {
   return (
     <Router>
       <div className="page-container">
+      <Navbar />
         <div className="content-wrap">
-          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" />
+            <Route path="/about" element={<About />} />
             <Route path="/contact" />
             <Route path="/locations" />
           </Routes>
