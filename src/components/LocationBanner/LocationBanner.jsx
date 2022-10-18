ecdc185697850d1311eedd1b46a5e19522b70873
Jane Vo <vo.janehanh@gmail.com>
2022-10-18 12:56:05 -0700

    Fix linting and formato on save

--- src/components/LocationBanner/LocationBanner.jsx
+++ src/components/LocationBanner/LocationBanner.jsx
@@ -1,36 +1,41 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import './LocationBanner.css';
 
-
 const LocationBanner = ({ city, image, clickable }) => {
-    const redirects = {
-        'San Francisco': 'san-francisco',
-        'New York': 'new-york',
-        'Irvine': 'irvine',
-    }
+  const redirects = {
+    'San Francisco': 'san-francisco',
+    'New York': 'new-york',
+    Irvine: 'irvine',
+  };
 
-    return (
-        <div className="location-banner" style={{backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-            {clickable && <a href={`/locations/${redirects[city]}`}>
-                <button className="banner-city">
-                    {city}
-                </button>
-            </a>}
-            {!clickable && <div className="banner-city">{city}</div>}
-        </div>
-    )
+  return (
+    <div
+      className="location-banner"
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {clickable && (
+        <a href={`/locations/${redirects[city]}`}>
+          <button className="banner-city">{city}</button>
+        </a>
+      )}
+      {!clickable && <div className="banner-city">{city}</div>}
+    </div>
+  );
 };
 
 export default LocationBanner;
 
 LocationBanner.defaultProps = {
-    clickable: false,
-}
+  clickable: false,
+};
 
 LocationBanner.propTypes = {
-    city: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    clickable: PropTypes.bool,
+  city: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  clickable: PropTypes.bool,
 };
-  
